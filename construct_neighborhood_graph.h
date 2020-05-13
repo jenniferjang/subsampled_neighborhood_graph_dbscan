83a16df8ab9734a7f940d88b88265660f64ff654
Jennifer Jang <jangj@uber.com>
2020-05-12 20:16:47 -0400

    Got rid of sets; use vectors now for speed

--- construct_neighborhood_graph.h
+++ construct_neighborhood_graph.h
@@ -39,9 +39,11 @@ pair< vector<int>, vector<float> > construct_neighborhood_graph_cy(int n,
 
     */
 
+    int[p * n * n] test;
     vector<vector< pair<int, float> > > neighbors_distances(n);
     float distance;
     float sq_eps = pow(eps, 2);
+    int neighbor;
 
     for (int i = 0; i < n; i++) {
         neighbors_distances[i] = vector< pair<int, float> >();
@@ -54,14 +56,14 @@ pair< vector<int>, vector<float> > construct_neighborhood_graph_cy(int n,
         // To ensure neighborhood graph is symmetric, we only sample points that come after
         for (int j = 0; j < floor(p * (n - i)); j++) {
 
-            int k = rand() % (n-i) + i;
-            distance = sq_euclidean_distance(d, i, k, X);
+            int neighbor = rand() % (n-i) + i;
+            distance = sq_euclidean_distance(d, i, neighbor, X);
 
             if (distance <= sq_eps) {
               
                 // Add edge between both vertices
-                neighbors_distances[i].push_back(make_pair(k, distance));
-                neighbors_distances[k].push_back(make_pair(i, distance));
+                neighbors_distances[i].push_back(make_pair(neighbor, distance));
+                neighbors_distances[neighbor].push_back(make_pair(i, distance));
             }
         }
 
