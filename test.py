2f3dda262c27731135c828f7a25324beaab6fe59
Jennifer Jang <j.jang42@gmail.com>
2020-06-28 17:56:34 -0400

    Got rid of for loops in subsampled_neighbors

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from scipy.sparse import csr_matrix
-from sklearn.metrics.pairwise import paired_distances
+from scipy.sparse import csr_matrix, coo_matrix
+from sklearn.metrics.pairwise import paired_distances, euclidean_distances
 import numpy as np
 
 def subsampled_neighbors(X, epsilon, s,
@@ -7,43 +7,30 @@ def subsampled_neighbors(X, epsilon, s,
                          metric='euclidean'):
   n = X.shape[0]
 
-  neighbors = np.array([], dtype=bool)
-  neighborptr = np.array([0], dtype=np.int32)
-  distances = np.array([], dtype=np.float)
+  # Sample the neighbors. Does not ensure that vertices are neighbors of itself
+  x = np.random.choice(np.arange(n), size=int(n * n * s), replace=True)
+  y = np.random.choice(np.arange(n), size=int(n * n * s), replace=True)
+  distances = paired_distances(X[x], X[y], metric=metric)
 
-  for i in range(X.shape[0]):
-    # Sample the neighbors
-    subsampled = np.append([i], np.random.choice(np.arange(i + 1, n), size=int((n - i -1) * s), replace=False))
-    
-    # This seems inefficient; I can just rewrite paired_distances to 
-    # use only one reference point, but it will be more code
-    dist = paired_distances([X[i]] * len(subsampled), X[subsampled], metric=metric)
-    
-    neighbors = np.append(neighbors, subsampled[dist <= epsilon])
-    neighborptr = np.append(neighborptr, len(neighbors))
+  neighbors_x = x[distances <= epsilon]
+  neighbors_y = y[distances <= epsilon]
 
-    if return_distances:
-      distances = np.append(distances, dist[dist <= epsilon])
-
-  neighborhood = csr_matrix((np.ones(len(neighbors)), neighbors, neighborptr), dtype=bool)
+  neighborhood = csr_matrix((np.ones(len(neighbors_x)), (neighbors_x, neighbors_y)), shape=(n, n), dtype=bool)
 
   # Make the matrix symmetric
   neighborhood_t = neighborhood.transpose()
   neighborhood += neighborhood_t
 
   if return_distances:
-    neighborhood_distances = csr_matrix((distances, neighbors, neighborptr), dtype=np.float)
-
-    # Make the matrix symmetric
-    neighborhood_distances_t = neighborhood_distances.transpose()
-    neighborhood_distances += neighborhood_distances_t
-    
-    return (neighborhood, neighborhood_distances)
+    rows, cols = neighborhood.nonzero()
+    neighb_dist = csr_matrix((paired_distances(X[rows], X[cols], metric=metric), (rows, cols)), shape=(n, n), dtype=np.float)
+    return (neighborhood, neighb_dist)
 
   else:
     return neighborhood
 
 X = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [6, 7, 8]])
+print(euclidean_distances(X))
 neighbors, distances = subsampled_neighbors(X, 4.0, 1.0, return_distances=True)
 print(neighbors.toarray())
 print(distances.toarray())
