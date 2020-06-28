ec68559bc6ca4da10c83fc459e4cbc5006560d8b
Jennifer Jang <j.jang42@gmail.com>
2020-06-28 13:12:46 -0400

    Added a test file for subsampled neighbors

---
+++ test.py
@@ -0,0 +1,49 @@
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import paired_distances
+import numpy as np
+
+def subsampled_neighbors(X, epsilon, s,
+                         return_distances=False,
+                         metric='euclidean'):
+  n, m = X.shape
+
+  neighbors = np.array([], dtype=bool)
+  neighborptr = np.array([0], dtype=np.int32)
+  distances = []
+
+  for i in range(n):
+    subsampled = np.append([i], np.random.choice(np.arange(i + 1, n), size=int((n - i -1) * s), replace=False))
+    
+    # This seems inefficient; I can just rewrite paired_distances to 
+    # use only one reference point, but it will be more code
+    dist = paired_distances([X[i]] * len(subsampled), X[subsampled], metric=metric)
+    
+    neighbors = np.append(neighbors, subsampled[dist <= epsilon])
+    neighborptr = np.append(neighborptr, len(neighbors))
+
+    if return_distances:
+      distances = np.append(distances, dist[dist <= epsilon])
+
+  neighborhood = csr_matrix((np.ones(len(neighbors), dtype=bool), 
+    neighbors, neighborptr))
+
+  # Make the matrix symmetric
+  neighborhood_t = neighborhood.transpose()
+  neighborhood += neighborhood_t
+
+  if return_distances:
+    neighborhood_distances = csr_matrix((distances, neighbors, neighborptr))
+
+    # Make the matrix symmetric
+    neighborhood_distances_t = neighborhood_distances.transpose()
+    neighborhood_distances += neighborhood_distances_t
+    
+    return (neighborhood, neighborhood_distances)
+
+  else:
+    return neighborhood
+
+X = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [6, 7, 8]])
+neighbors, distances = subsampled_neighbors(X, 4.0, 1.0, return_distances=True)
+print(neighbors.toarray())
+print(distances.toarray())
